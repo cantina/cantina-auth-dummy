f67636d8c147aabd042719f8220d1d26ba0ac6d1
Carlos Rodriguez <carlos@s8f.org>
2012-05-23 18:47:11 -0700

    Use new cantina middleware plugin

--- test/app.js
+++ test/app.js
@@ -16,15 +16,20 @@ app.use(require('../').plugin, {
   consumerKey: '884d36536702bfca872feb5b4e43ff3a',
   consumerSecret: 'd416fae3609ecdd578548071253273c0',
   callbackURL: 'http://localhost:3000/auth/freedomworks/callback',
-  authURL: '/',
+  authURL: '/login',
   verify: function(token, tokenSecret, profile, done) {
     done(null, profile);
   }
 });
 
 app.router.get('/', function() {
-  this.res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
-  this.res.end('Welcome, ' + this.req.user.displayName + '!');
+  this.res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+  if (this.req.isAuthenticated()) {
+    this.res.end('Welcome, <a href="http://connect.freedomworks.org/user/' + this.req.user.id + '">' + this.req.user.displayName + '</a>!');
+  }
+  else {
+    this.res.end('<a href="/login">click here to login via FreedomWorks</a>');
+  }
 });
 
 app.start(3000);
